5fca7afffc7898bdbb5ceef0ab314f6ac78a1039
parjanyaroy <parjanyaroy@gmail.com>
2018-08-26 13:55:27 +0530

    Calling time API to get timestamp for encryption

--- NodeMcu/testSaleforce2/testSaleforce2.ino
+++ NodeMcu/testSaleforce2/testSaleforce2.ino
@@ -2,10 +2,23 @@
 #include <WiFiManager.h>
 #include <ESP8266HTTPClient.h>
 
+// -------------------------------- TIME ZONE AUTHENTICATION DATA --------------------------------------------//
+char dateApiName[]="http://api.timezonedb.com/v2/get-time-zone";
+char apiKey[]="RKEX6GHOJMTH";
+char zone[]="Asia/Kolkata";
+// -------------------------------- TIME ZONE AUTHENTICATION DATA --------------------------------------------//
 
+
+// -------------------------------- SALESFORCE SERVER DATA --------------------------------------------//
 char servername[]="homeautomationalpha-developer-edition.ap4.force.com";              // remote server we will connect to
+// -------------------------------- SALESFORCE SERVER DATA --------------------------------------------//
+
+// -------------------------------- AUTO CONNECT MANAGER DATA --------------------------------------------//
 char AccessPointName[]="Home Automation Setup";
 char AccessPointPassword[]="Home@1234";
+// -------------------------------- AUTO CONNECT MANAGER DATA --------------------------------------------//
+
+
 String result;
 
 int  counter = 60;                                      
@@ -29,7 +42,21 @@ const char* _getLink = "http://homeautomationalpha-developer-edition.ap4.force.c
 void ConnectToCloud()
 {
    HTTPClient http;
+    //http.begin(dateApiName.."?key="..apiKey+"&format=json&by=zone&zone="..zone);
+    http.begin("http://api.timezonedb.com/v2/get-time-zone?key=RKEX6GHOJMTH&format=json&by=zone&zone=Asia/Kolkata");
+    int httpCode1 = http.GET();
+    if (httpCode1 > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode1);
+      if (httpCode1 == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode1).c_str());
+    }
+    http.end();
 
+    
     Serial.print("[HTTP] begin...\n"); 
     http.begin("https://homeautomationalpha-developer-edition.ap5.force.com/services/apexrest/iotservice?DeviceId=1231231231242312312","D1 A3 3C D7 D5 87 0A 10 81 22 BF 44 12 B8 C8 7B 1A D2 DC 94");
     Serial.print("[HTTP] GET...\n");
