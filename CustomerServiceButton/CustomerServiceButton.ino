9278c9a2d19a3ede8227af0096c421bf8af757e5
parjanyaroy <parjanyaroy@gmail.com>
2019-05-01 19:31:41 +0530

    Salesforce Connectivity Complete.Char array needs to be fixed

--- CustomerServiceButton/CustomerServiceButton.ino
+++ CustomerServiceButton/CustomerServiceButton.ino
@@ -18,6 +18,7 @@ char AccessPointPassword[] = "Home@1234";
 
 // -------------------------------- AUTO CONNECT DEVICE ID DATA--------------------------------------------//
 String server = "https://sandbox-parjanyaroy-developer-edition.cs58.force.com/services/apexrest/iotservice?deviceid="; //DIRECT SFDC CONNECT
+String serverURL =""; 
 char shaFingerPrint[]="EF C6 00 66 2D 39 38 F8 D6 FD 80 81 63 90 43 CF 01 91 D4 79";
 //char shaFingerPrint[]="08 3B 71 72 02 43 6E CA ED 42 86 93 BA 7E DF 81 C4 BC 62 30";
 
@@ -39,150 +40,110 @@ const char *DeviceIdInput;
 boolean isResetWifiSettings=true;
 
 
-struct DeviceData {
-  boolean isResetWifiSettings=true;
-  const char *deviceId;
-};
+
 
 void setup()
 {
   Serial.begin(9600);
-  
-  struct DeviceData fetchedData=getDeviceData();
-  getDeviceData();
   pinMode(executingSetup, OUTPUT);      // declare LED as output
   pinMode(caseRegistered, OUTPUT);      // declare LED as output
-  pinMode(reportIssueButton, INPUT);     // declare sensor as input
+  pinMode(reportIssueButton, INPUT);    // declare sensor as input
   digitalWrite(executingSetup, HIGH);
-  Serial.println("");
-  if(isSetupDebug==true){Serial.println("[LOC] Booting Node MCU ");}
-  
+  if(isSetupDebug==true){Serial.println("[setup] Booting Node MCU ");}
   WiFiManager wifiManager;
   if(isResetWifiSettings){wifiManager.resetSettings();}
-  
- // wifiManager.setDebugOutput(isSetupDebug);
+  wifiManager.setDebugOutput(false);
   WiFiManagerParameter custom_device_id_hardware("server", "Device Id", DeviceIdInput, 10);
   wifiManager.addParameter(&custom_device_id_hardware);
   wifiManager.autoConnect(AccessPointName, AccessPointPassword);
   DeviceIdInput = custom_device_id_hardware.getValue();
-  if(isSetupDebug==true){Serial.printf("[LOC] Device Key entered: %s\n", DeviceIdInput);}
+  if(isSetupDebug==true){Serial.printf("[setup] Device Key entered: %s\n", DeviceIdInput);}
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  if(isSetupDebug==true){Serial.println("[LOC] Attempting to connect to wifi"); }
+  delay(500);
+  if(isSetupDebug==true){Serial.println("[setup] Attempting to connect to wifi"); }
   }
-  if(isSetupDebug==true){Serial.println("[LOC] Connected To Wifi");}
+  if(isSetupDebug==true){Serial.println("[setup] Connected To Wifi");}
   boolean authenticated=false;
-  while(authenticated==false)
-  {
-  if(isSetupDebug==true){Serial.println("[LOC] Attempting to authenticate and connect to server");}
+  while(authenticated==false){
+  if(isSetupDebug==true){Serial.println("[setup] Attempting to authenticate and connect to server");}
   authenticated=ConnectToCloud();
   delay(1000); 
   }
-
-  DeviceData customVar ;
-  customVar.isResetWifiSettings=false;
-  customVar.deviceId=DeviceIdInput;
-  setDeviceData(customVar);
-  getDeviceData();
-  //digitalWrite(output_Boot, LOW);
-  digitalWrite(executingSetup, LOW);
-   
+  digitalWrite(executingSetup, LOW);   
 }
-
-void setDeviceData(struct DeviceData record)
-{
-  Serial.printf("\nEEPROM:: PUT : isReset: %d deviceId: %s",record.isResetWifiSettings,record.deviceId);
-  EEPROM.begin(512);
-  EEPROM.put(0, record);
-  EEPROM.commit();
-}
-
-struct DeviceData getDeviceData()
-{
-  struct DeviceData fetchedRecord;
-  EEPROM.get( 0, fetchedRecord );
-  Serial.printf("\nEEPROM:: GET : isReset: %d deviceId: %s",fetchedRecord.isResetWifiSettings,fetchedRecord.deviceId);
-  return fetchedRecord;
-}
-
 
 boolean ConnectToCloud()
 {
   HTTPClient http;
   char payload[400];
   char requestURLSF[150];
-  
-  // Logic to generate the master token
-  String dataToken = DeviceIdInput;// First 20 characters will be the device Id , remaining will be the timestamp
-  dataToken.replace("0","s");
-  dataToken.replace("1","u");
-  dataToken.replace("2","c");
-  dataToken.replace("3","h");
-  dataToken.replace("4","a");
-  dataToken.replace("5","n");
-  dataToken.replace("6","d");
-  dataToken.replace("7","r");
-  dataToken.replace("8","p");
-  dataToken.replace("9","l");
-  if(isSetupDebug==true){Serial.print("[LOC] Datatoken \n"+dataToken);}
-  if(isSetupDebug==true){Serial.print("[LOC] Connecting To Server \n");}
-  server=server+dataToken;
-  server.toCharArray(requestURLSF,server.length()+1);
+  if(isSetupDebug==true){Serial.printf("[ConnectToCloud] DeviceIdInput ::%s\n",DeviceIdInput);}
+  if(isSetupDebug==true){Serial.print("[ConnectToCloud] Connecting To Server \n");}
+  serverURL=server+DeviceIdInput;
+  serverURL.toCharArray(requestURLSF,serverURL.length()+1);
   if(isSetupDebug==true){Serial.printf("[LOC] Request URL  : %s\n", requestURLSF);}
   http.begin(requestURLSF,shaFingerPrint);
   int httpCode = http.GET();
   if (httpCode > 0) {
-    if(isSetupDebug==true){Serial.printf("[LOC] Connection To Server Successful : %d\n", httpCode);}
+    if(isSetupDebug==true){Serial.printf("[ConnectToCloud] Connection To Server Successful : %d\n", httpCode);}
    // if (httpCode == HTTP_CODE_OK) {
       String payloadString = http.getString();
       payloadString.toCharArray(payload, payloadString.length() + 1);
-      if(isSetupDebug==true){Serial.printf("[LOC] PAYLOAD : %s\n", payload);}
+      if(isSetupDebug==true){Serial.printf("[ConnectToCloud] PAYLOAD : %s\n", payload);}
     
   // }
   } 
   else {
-    if(isSetupDebug==true){Serial.printf("[LOC] Failed to connect to server, error: %s\n", http.errorToString(httpCode).c_str());}
+    if(isSetupDebug==true){Serial.printf("[ConnectToCloud] Failed to connect to server, error: %s\n", http.errorToString(httpCode).c_str());}
     return false;
   }
   http.end();
-  if(isSetupDebug==true){Serial.println("[LOC] ConnectToCloud Successful");}
+  if(isSetupDebug==true){Serial.println("[ConnectToCloud] ConnectToCloud Successful");}
   delay(500);
   return true;
   }
-  int counter=0;
+  
+  
+int counter=0; // Counter for loop to send ping at 1 minute interval
 void loop(){
      temp = digitalRead(reportIssueButton);
-     if (temp == HIGH) {
-        Serial.println("Send Call To Register Case");
+     if(temp == HIGH){
+        if(isSetupDebug==true){Serial.printf("[loop] Send Call To Register Case");}
+        pingCloud("ERROR");
         digitalWrite(caseRegistered, HIGH);
         delay(3000);
         digitalWrite(caseRegistered, LOW);
        }
-
-       if(counter>=600)
-       {
-        Serial.println("Send PING to salesforce");
+       if(counter>=60){
+        if(isSetupDebug==true){Serial.printf("[loop] Send PING to salesforce");}
+        pingCloud("PING");
         counter=0;
        }
-       else
-       {
+       else{
         counter++;
         delay(100);
        }
-     
-  //Serial.println("Loop starts");
-    // if button pressed 
-  //{
-  //  -> send request to salesforce for registering case
-  // -> read response on successful case registration
-  // switch on the led 
-  //-> set case registered url
-  // delay for 2 seconds to make sure button is not read continuously
-  // switch off the led
-  //}
-  // if counter is 600
-  // { send status ping to salesforce , set counter to 0}
-  // else
-  // { increment counter , delay 100 milisecond second}
+       // Code a structure for resetting connection incase of connection loss
 }
+boolean pingCloud(char *message)
+{
+  HTTPClient http;
+  char payload[400];
+  char requestURLSF[150];
+  if(isSetupDebug==true){Serial.printf("[pingCloud] DeviceIdInput ::%s\n",DeviceIdInput);}
+  serverURL=server+DeviceIdInput+"&process="+message;
+  serverURL.toCharArray(requestURLSF,serverURL.length()+1);
+  if(isSetupDebug==true){Serial.printf("[pingCloud] Request URL  : %s\n", requestURLSF);}
+  http.begin(requestURLSF,shaFingerPrint);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    if(isSetupDebug==true){Serial.printf("[pingCloud] Connection To Server Successful : %d\n", httpCode);}
+      String payloadString = http.getString();
+      payloadString.toCharArray(payload, payloadString.length() + 1);
+      if(isSetupDebug==true){Serial.printf("[pingCloud] PAYLOAD : %s\n", payload);}
+      serverURL="";
+  }
+}
+
+
   
